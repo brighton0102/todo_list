05ff195eeb7b12160834395826e6611e1b6070cc
brighton0102 <mtandibrighton0102@gmail.com>
2023-08-26 14:24:49 +0200

    Fix linters

--- src/modules/genenerate_function.js
+++ src/modules/genenerate_function.js
@@ -1,17 +1,17 @@
 export const addNewItem = (itemDescription, itemsLength) => {
-    const newItem = {
-        description: itemDescription,
-        completed: false,
-        index: itemsLength + 1
-    };
-    return newItem;
+  const newItem = {
+    description: itemDescription,
+    completed: false,
+    index: itemsLength + 1,
+  };
+  return newItem;
 };
 
 export const removeItems = (items, index) => {
-    items.splice(index, 1);
-    let i = index;
-    while (i < items.lenghth) {
-        items[i].index = i + 1;
-        i += 1;
-    }
+  items.splice(index, 1);
+  let i = index;
+  while (i < items.lenghth) {
+    items[i].index = i + 1;
+    i += 1;
+  }
 };
