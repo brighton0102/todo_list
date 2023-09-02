128d2cadbc869a5f42e3fe02ed1958df6b70f47e
brighton0102 <mtandibrighton0102@gmail.com>
2023-09-02 15:35:37 +0200

    Add test for updateCompletedStatus file

---
+++ src/updateCompletedStatus.test.js
@@ -0,0 +1,42 @@
+import updateCompletedStatus from "./updateCompletedStatus.js";
+
+describe("updateCompletedStatus function", () => {
+  it("should update the completed status of an item to true", () => {
+    const items = [
+      { description: "Task 1", completed: false },
+      { description: "Task 2", completed: false },
+    ];
+    const indexToUpdate = 0;
+    const newStatus = true;
+
+    const updatedItems = updateCompletedStatus(items, indexToUpdate, newStatus);
+
+    expect(updatedItems[indexToUpdate].completed).toBe(newStatus);
+  });
+
+  it("should update the completed status of an item to false", () => {
+    const items = [
+      { description: "Task 1", completed: true },
+      { description: "Task 2", completed: true },
+    ];
+    const indexToUpdate = 1;
+    const newStatus = false;
+
+    const updatedItems = updateCompletedStatus(items, indexToUpdate, newStatus);
+
+    expect(updatedItems[indexToUpdate].completed).toBe(newStatus);
+  });
+
+  it("should not modify items if the index is out of bounds", () => {
+    const items = [
+      { description: "Task 1", completed: false },
+      { description: "Task 2", completed: false },
+    ];
+    const indexToUpdate = 10;
+    const newStatus = true;
+
+    const updatedItems = updateCompletedStatus(items, indexToUpdate, newStatus);
+
+    expect(updatedItems).toEqual(items);
+  });
+});
