29e6c780dd8843e93d52ce5cebf81e40c073c659
efmomoh <efmomoh25@gmail.com>
2023-09-02 13:46:40 +0000

    Modifies the source folder

--- src/updateCompletedStatus.test.js
+++ src/updateCompletedStatus.test.js
@@ -1,10 +1,10 @@
-import updateCompletedStatus from "./updateCompletedStatus.js";
+import updateCompletedStatus from './updateCompletedStatus.js';
 
-describe("updateCompletedStatus function", () => {
-  it("should update the completed status of an item to true", () => {
+describe('updateCompletedStatus function', () => {
+  it('should update the completed status of an item to true', () => {
     const items = [
-      { description: "Task 1", completed: false },
-      { description: "Task 2", completed: false },
+      { description: 'Task 1', completed: false },
+      { description: 'Task 2', completed: false },
     ];
     const indexToUpdate = 0;
     const newStatus = true;
@@ -14,10 +14,10 @@ describe("updateCompletedStatus function", () => {
     expect(updatedItems[indexToUpdate].completed).toBe(newStatus);
   });
 
-  it("should update the completed status of an item to false", () => {
+  it('should update the completed status of an item to false', () => {
     const items = [
-      { description: "Task 1", completed: true },
-      { description: "Task 2", completed: true },
+      { description: 'Task 1', completed: true },
+      { description: 'Task 2', completed: true },
     ];
     const indexToUpdate = 1;
     const newStatus = false;
@@ -27,10 +27,10 @@ describe("updateCompletedStatus function", () => {
     expect(updatedItems[indexToUpdate].completed).toBe(newStatus);
   });
 
-  it("should not modify items if the index is out of bounds", () => {
+  it('should not modify items if the index is out of bounds', () => {
     const items = [
-      { description: "Task 1", completed: false },
-      { description: "Task 2", completed: false },
+      { description: 'Task 1', completed: false },
+      { description: 'Task 2', completed: false },
     ];
     const indexToUpdate = 10;
     const newStatus = true;
